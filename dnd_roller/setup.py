bf9d626d62d492433c321c2e049fd3d24799959f
bikegeek <minnawin@ucar.edu>
2023-10-10 12:18:08 -0600

    dnd-roller v0.01

---
+++ dnd_roller/setup.py
@@ -0,0 +1,25 @@
+# file: setup.py
+
+import os
+from pathlib import Path
+from setuptools import find_packages, setup
+
+PKG_FOLDER = Path(os.path.abspath(os.path.dirname(__file__)))
+
+with open(PKG_FOLDER / "README.md") as f:
+    long_description = f.read()
+
+
+setup(
+    name="dnd-roller",
+    version="0.0.1",
+    author= "Valerio Maggio",
+    author_email="vmaggio@anaconda.com",
+    description="Python package to roll D&D dice in the terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/YOUR-GITHUB_ID/dnd-roller",
+    include_package_data=True,
+    packages=find_packages(exclude=[]),
+    install_requires=["tabulate"],
+)
